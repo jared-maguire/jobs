a9c1216ab29da582a22a646d04ebd8ddc0af61fa
jared-maguire <jared.maguire@gmail.com>
2022-06-18 09:25:19 -0700

    Update jobs_app.py

--- jobs_app.py
+++ jobs_app.py
@@ -91,20 +91,5 @@ def dump():
     return data
 
 
-"""
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
-"""
-
 if __name__ == "__main__":
     app.run()
