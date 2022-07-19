ceef593a09e5e533a915fad5bc1b0fff2fcc52f6
jared-maguire <jared.maguire@gmail.com>
2022-07-19 08:37:59 -0700

    one more test

--- test_k8s.py
+++ test_k8s.py
@@ -3,9 +3,13 @@
 import k8s
 
 
+## Cluster
+
 def test_cluster_config():
     assert(k8s.check_cluster_config())
 
+
+## Jobs
 
 def test_run_and_wait():
     result = k8s.wait(k8s.run(lambda: "Hooray"), timeout=30)
@@ -52,6 +56,8 @@ def test_ngs_workflow():
     assert(results.__class__ == dict)
 
 
+## Volumes
+
 def test_volumes():
     def wf():
         import json
@@ -72,3 +78,37 @@ def test_volumes():
 
     result = wf()
     assert(result == "hey")
+
+
+def test_rwx_volumes():
+    import time
+
+    def wf():
+        volume = k8s.create_volume("1Mi")
+
+        def waiter():
+            import json, os, time
+            iteration_count = 0
+            while True:
+                if os.path.exists(f"/mnt/{volume}/test.json"):
+                    with open(f"/mnt/{volume}/test.json") as fp:
+                        data = json.load(fp)
+                        return dict(iteration_count=iteration_count, data=data)
+                time.sleep(1)
+                iteration_count += 1
+
+        def writer():
+            import json, os
+            with open(f"/mnt/{volume}/test.json", "w") as fp:
+                json.dump("the writer has writ", fp)
+            return True
+
+        job1 = k8s.run(waiter, volumes=[volume])
+        time.sleep(5)
+        job2 = k8s.run(writer, volumes=[volume])
+        k8s.wait(job2)
+        result = k8s.wait(job1)
+        return result
+
+    result = wf()
+    assert((result["iteration_count"] >= 0) and (result["data"] == "the writer has writ"))
