761afb11547984a35dd0b84e8b30db3c18515bea
jared-maguire <jared.maguire@gmail.com>
2023-04-21 07:25:07 -0700

    no options to this one

--- sk8s/clouds/local.py
+++ sk8s/clouds/local.py
@@ -16,6 +16,6 @@ import sk8s.configs
 
 # Overall Local cluster config:
 
-def config_cluster(project, dryrun=False):
+def config_cluster():
     sk8s.configs.save_config(sk8s.default_config)
     return sk8s.default_config
