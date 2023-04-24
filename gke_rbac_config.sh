c55d2572c132abe0b07ddea65ebf64f3e6da270a
jared-maguire <jared.maguire@gmail.com>
2023-04-24 09:04:25 -0700

    this is eldrich knowledge, guard it with your life

---
+++ gke_rbac_config.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# from: https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity
+
+NS=sk8s
+KSA_NAME=internal-kubectl
+
+GSA_NAME=261248090722-compute@developer.gserviceaccount.com
+BUCKET_NAME=jared-genome
+PROJECT_ID="jared-genome-analysis"
+
+#kubectl create serviceaccount $KSA_NAME -n $NS
+
+#gcloud iam service-accounts create $GSA_NAME \
+#    --project=$PROJECT_ID
+
+gcloud storage buckets add-iam-policy-binding gs://$BUCKET_NAME \
+  --member "serviceAccount:$GSA_NAME" \
+  --role=roles/storage.objectViewer
+
+gcloud iam service-accounts add-iam-policy-binding $GSA_NAME \
+  --role roles/iam.workloadIdentityUser \
+  --member "serviceAccount:$PROJECT_ID.svc.id.goog[$NS/$KSA_NAME]"
+
+kubectl annotate serviceaccount $KSA_NAME \
+    --namespace $NS \
+    261248090722-compute@developer.gserviceaccount.com
+    
+#    iam.gke.io/gcp-service-account=$GSA_NAME@$PROJECT_ID.iam.gserviceaccount.com
