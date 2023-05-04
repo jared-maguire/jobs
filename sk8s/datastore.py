89353f07f52f8f3efef67372b3df870513bdb5dc
jared-maguire <jared.maguire@gmail.com>
2023-05-04 08:59:48 -0700

    just about the simplest nosql DB ever

---
+++ sk8s/datastore.py
@@ -0,0 +1,40 @@
+
+## Ultra-lightweight key/value store.
+
+import sk8s
+import subprocess
+from flask import Flask
+from flask import request
+import requests
+import json
+
+
+class Datastore:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.data = dict()
+        self.app.add_url_rule('/put/<key>', 'put', self.put, methods=["POST"])
+        self.app.add_url_rule('/get/<key>', 'get', self.get, methods=["GET"])
+    
+    def put(self, key):
+        data = request.data
+        self.data[key] = json.loads(data.decode("utf-8"))
+        print(self.data)
+        return "OK"
+
+    def get(self, key):
+        return json.dumps(self.data[key])
+
+    def run(self):
+        return self.app.run()
+
+
+def put(url, key, val):
+    r = requests.post(f"{url}/put/{key}", json=val)
+    return r.content.decode("utf-8")
+
+
+def get(url, key):
+    r = requests.get(f"{url}/get/{key}")
+    return json.loads(r.content.decode("utf-8"))
+
