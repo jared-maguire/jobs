06c68c9f01fa24ae3fc77810f4611212c0275f0d
jared-maguire <jared.maguire@gmail.com>
2023-08-15 08:58:24 -0700

    meaningless cleanup

--- test_k8s.py
+++ test_k8s.py
@@ -76,8 +76,7 @@ def test_simple_workflow():
         jobs1 = sk8s.map(lambda i: i, range(3), asynchro=True)
         return sk8s.wait(sk8s.run(lambda inputs: sum(inputs),
                                   map(sk8s.wait, jobs1),
-                                  name="job2-{s}",
-                                  asynchro=True),
+                                  name="job2-{s}"),
                          timeout=500)
     result = sk8s.wait(sk8s.run(wf, name="wf-{s}"), timeout=500)
     assert(result == 3)
@@ -390,9 +389,9 @@ def test_service():
     sk8s.services.shutdown_service(service_name)
 
     # And finally, check that the result is correct
-    #resp = json.loads(response.content)
-    #assert((resp["message"] == "awesome") and
-    #       (resp["response"] == "awesome awesome"))
+    resp = json.loads(response.content)
+    assert((resp["message"] == "awesome") and
+           (resp["response"] == "awesome awesome"))
 
 
 @pytest.mark.services
